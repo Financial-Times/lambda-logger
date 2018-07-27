101c5b47424052d0b43508c3bfbec78af142f28f
Charlie Briggs <charlie.briggs@ft.com>
2018-07-27 16:05:04 +0100

    Try using awslambda.sendConsoleLogs internal call if it exists

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@financial-times/de-tooling', 'prettier'],
     plugins: ['prettier'],
+    globals: { awslambda: true },
     rules: {
         'prettier/prettier': 'error',
     },
